6fcfec3d2015b62d6ee04c6a47bdad460dbe6f91
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2022-09-19 14:35:25 +0900

    Add files via upload

---
+++ HTML/0919/fruit.js
@@ -0,0 +1,11 @@
+function over(obj) {
+			obj.src="../images/banana.png";
+				   }
+
+function out(obj)  {
+			obj.src="../images/apple.png";
+				   }
+
+function click2(obj)  {
+			obj.src="../images/back.png";
+				   }
